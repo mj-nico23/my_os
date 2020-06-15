525d103f1bb8ab109ad01654b1c8d72a9d792043
Nico Graglia <nicograglia@gmail.com>
2020-06-15 20:37:27 -0300

    tests part completed

--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -7,26 +7,20 @@
 use core::panic::PanicInfo;
 use my_os::println;
 
-#[no_mangle]
+#[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
-    println!("Hello World{}", "!");
-
-    #[cfg(test)]
     test_main();
 
     loop {}
 }
 
-/// This function is called on panic.
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
-    loop {}
-}
 
-#[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     my_os::test_panic_handler(info)
+}
+
+#[test_case]
+fn test_println() {
+    println!("test_println output");
 }
